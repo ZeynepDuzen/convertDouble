9ce1b1f265ac63b6acf463e71fbf51e27509f4e5
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-16 15:17:54 +0300

    Proje dosyası ekle.

---
+++ convertDouble/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace convertDouble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //klavyeden girilen ondalıklı sayıyı ekrana yazdıran kod bloğu
+            double sayi;
+            Console.WriteLine("Lütfen ondalıklı bir sayi giriniz : ");
+            Console.WriteLine();
+            sayi = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Girmis oldugunuz ondalıklı sayi : " + sayi);
+
+            Console.Read();
+        }
+    }
+}
